c55aadcc6005ab55f288a1f9e451716a097b6b45
Saya Funao <2022pyt41-student002@la-study.com>
2022-05-23 16:39:18 +0900

    データベースに接続

--- migrations/versions/768fd2ab93f7_.py
+++ migrations/versions/768fd2ab93f7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 51719aa5cf25
+Revision ID: 768fd2ab93f7
 Revises: 
-Create Date: 2022-05-23 14:19:57.215067
+Create Date: 2022-05-23 16:34:46.678341
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '51719aa5cf25'
+revision = '768fd2ab93f7'
 down_revision = None
 branch_labels = None
 depends_on = None
