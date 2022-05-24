f4e297527180d87d0be8482baf7fbbd39de4b74e
Rin Gamada <2022py41-student008@la-study.com>
2022-05-24 15:18:14 +0900

    book_management.py作成しました

---
+++ app/views/book_management.py
@@ -0,0 +1,59 @@
+from flask import render_template, request, url_for, session, redirect, flash, Blueprint
+
+import datetime
+
+from lib.models import Book
+
+from lib.db import db
+
+
+book_management = Blueprint('book_management', __name__)
+
+#選択された教科書の情報変更ページを表示
+@book_management.route('/book_info_change/show')
+def book_info_show(id):
+    book_info=Book.query.get(id)
+    return render_template('book_info_change_admin.html', book_info=book_info)
+
+#検索結果に一致する教科書情報を一覧表示する
+@book_management.route('/book_info_change/search', methods=['POST'])
+def book_search():
+    searched_title = request.form.get('search')
+    if searched_title == '':
+        book_info = Book.query.all()
+    else:
+        book_info = Book.query.filter(Book.title==searched_title)
+    return render_template('book_management.html', book_info=book_info)
+
+#選択された教科書を削除し、教科書一覧を再表示する
+@book_management.route('/book_info_change/delete', methods=['POST'])
+def book_delete(id):
+    book_info = Book.query.get(id)
+    db.session.delete(book_info)
+    db.session.commit()
+    return redirect(url_for('book_management.book_search'))
+
+#選択された教科書内容を入力内容に更新し、一覧を再表示する
+@book_management.route('/book_info_change/update', methods=['POST'])
+def book_update(id):
+    book_info = Book.query.get(id)
+    book_info.isbn_no = request.form.get('isbn_no')
+    book_info.title = request.form.get('title')
+    book_info.author = request.form.get('author')
+    book_info.publisher = request.form.get('publisher')
+    book_info.price = request.form.get('price')
+    book_info.category = request.form.get('category')
+    book_info.status = request.form.get('status')
+    book_info.updated = datetime.datetime.now()
+
+    try:
+        db.session.merge(book_info)
+        db.session.commit()
+    except:
+        flash('入力した内容を再度確認してください')
+        return redirect(url_for('book_management.book_info_show', id=id))
+    return redirect(url_for('book_management.book_search'))
+
+
+
+
