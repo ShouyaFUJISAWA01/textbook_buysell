9e12ece052cdcef00a090a65c0d4261d0eb24381
Ryotaro Kamikubo <2022pyt41-student20@la-study.com>
2022-05-24 16:05:48 +0900

    book_registerを変更

--- app/views/book_register.py
+++ app/views/book_register.py
@@ -14,7 +14,7 @@ book_register = Blueprint('book_register', __name__)
 def book_register():
     book=Book(
         name=request.form.get('name'),
-        user_id=session.get('auth_id'),
+        user_id=session.get('user_id'),
         isbd_no=request.form.get('isbn_no'),
         author=request.form.get('author'),
         company=request.form.get('company'),
