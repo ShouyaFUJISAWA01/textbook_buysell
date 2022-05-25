f0c56c1fce9419e0d08f2d873786eb39b10c3335
Ryotaro Kamikubo <2022pyt41-student20@la-study.com>
2022-05-25 11:59:38 +0900

    init.pyの更新

--- app/views/homes.py
+++ app/views/homes.py
@@ -8,10 +8,10 @@ from lib.models import User, Book
 from lib.db import db
 
 # Blueprintでitemアプリケーションを登録
-home = Blueprint('home', __name__)
+home_bp = Blueprint('home', __name__)
 
 #購入ページの表示
-@home.route('/buy')
+@home_bp.route('/buy')
 def buy():
     user_id=session("user_id")
     items=Book.query.filter(Book.user_id==user_id)#データベースBookクラスから一覧を持ってくる
@@ -19,34 +19,34 @@ def buy():
 
 
 #教科書登録ページの表示
-@home.route('/book_register')
+@home_bp.route('/book_register')
 def book_register():
     return render_template('homes/book_register.html')
 
 
 #教科書情報変更ページの表示
-@home.route('/book_info_change/<int:id>')
+@home_bp.route('/book_info_change/<int:id>')
 def book_info_change(id):
     book_change=Book.query.get(id)
     return render_template('homes/book_info_change.html', book_change=book_change)
 
 
 #会員情報変更ページの表示
-@home.route('/user_info_change/<int:id>')
+@home_bp.route('/user_info_change/<int:id>')
 def user_info_change(id):
     user_change=User.query.get(id)#idと一致するもの
     return render_template('homes/user_info_change.html', user_change=user_change)
 
 
 #会員管理ページの表示
-@home.routte('/user_management')
+@home_bp.routte('/user_management')
 def user_management():
     users=User.query.order_by(User.id.desc()).all()
     return render_template('homes/user_management.html', users=users)
 
 
 #教科書管理ページの表示
-@home.route('/book_management')
+@home_bp.route('/book_management')
 def book_management():
     books=Book.query.all()
     return render_template('homes/book_management.html', books=books)
--- app/views/user_info_change.py
+++ app/views/user_info_change.py
@@ -7,11 +7,11 @@ from lib.models import User, Book
 from lib.db import db
 
 
-user_info_change = Blueprint('user_info_change', __name__)
+user_info_change_bp = Blueprint('user_info_change', __name__)
 
 
 # 以下で使うidを後ほどHTMLで指定する
-@user_info_change.route('/user_info_change/update/<int:id>', methods=['POST'])
+@user_info_change_bp.route('/user_info_change/update/<int:id>', methods=['POST'])
 def user_info_change(id):
     user_info = User.query.get(id)
     user_info.name = request.form.get('name')
@@ -28,7 +28,7 @@ def user_info_change(id):
     return redirect(url_for('top.home'))
 
 # 以下で使うidを後ほどHTMLで指定する
-@user_info_change.route('/home/delete/<int:id>', methods=['POST'])
+@user_info_change_bp.route('/home/delete/<int:id>', methods=['POST'])
 def user_info_delete(id):
     user_info = User.query.get(id)
     db.session.delete(user_info)
