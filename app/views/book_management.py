23d09487c12e9ca40e1366201f6362642ba1da22
ShouyaFUJISAWA01 <shouya0520@yahoo.co.jp>
2022-05-26 14:50:46 +0900

    コードを修正

--- app/views/book_management.py
+++ app/views/book_management.py
@@ -10,35 +10,35 @@ from lib.db import db
 book_management = Blueprint('book_management', __name__)
 
 #選択された教科書の情報変更ページを表示
-@book_management.route('/book_info_change/show')
+@book_management.route('/book_info_change/show/<int:id>')
 def book_info_show(id):
     book_info=Book.query.get(id)
-    return render_template('book_info_change_admin.html', book_info=book_info)
+    return render_template('book_management/book_info_change_admin.html', book_info=book_info)
 
 #検索結果に一致する教科書情報を一覧表示する
 @book_management.route('/book_info_change/search', methods=['POST'])
 def book_search():
     searched_title = request.form.get('search')
     if searched_title == '':
-        book_info = Book.query.all()
+        books = Book.query.all()
     else:
-        book_info = Book.query.filter(Book.title==searched_title)
-    return render_template('book_management.html', book_info=book_info)
+        books = Book.query.filter(Book.title==searched_title).all()
+    return render_template('book_management/book_management.html', books=books)
 
 #選択された教科書を削除し、教科書一覧を再表示する
-@book_management.route('/book_info_change/delete', methods=['POST'])
+@book_management.route('/book_info_change/admin_delete/<int:id>', methods=['POST'])
 def book_delete(id):
     book_info = Book.query.get(id)
     db.session.delete(book_info)
     db.session.commit()
-    return redirect(url_for('book_management.book_search'))
+    return redirect(url_for('home.book_management'))
 
 #選択された教科書内容を入力内容に更新し、一覧を再表示する
-@book_management.route('/book_info_change/update', methods=['POST'])
+@book_management.route('/book_info_change/admin_update/<int:id>', methods=['POST'])
 def book_update(id):
     book_info = Book.query.get(id)
-    book_info.isbn_no = request.form.get('isbn_no')
     book_info.title = request.form.get('title')
+    book_info.isbn_no = request.form.get('isbn')
     book_info.author = request.form.get('author')
     book_info.publisher = request.form.get('publisher')
     book_info.price = request.form.get('price')
@@ -46,13 +46,13 @@ def book_update(id):
     book_info.status = request.form.get('status')
     book_info.updated = datetime.datetime.now()
 
-    try:
-        db.session.merge(book_info)
-        db.session.commit()
-    except:
-        flash('入力した内容を再度確認してください')
-        return redirect(url_for('book_management.book_info_show', id=id))
-    return redirect(url_for('book_management.book_search'))
+    # try:
+    db.session.merge(book_info)
+    db.session.commit()
+    # except:
+    #     flash('入力した内容を再度確認してください')
+    #     return redirect(url_for('book_management.book_info_show', id=id))
+    return redirect(url_for('home.book_management'))
 
 
 
