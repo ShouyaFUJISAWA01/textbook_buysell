045e44055afa35781b2c726b4c4412f3d8dbb865
Ryotaro Kamikubo <2022pyt41-student20@la-study.com>
2022-05-27 18:00:02 +0900

    会員、教科書の登録に確認画面をつけました。

--- app/views/book_register.py
+++ app/views/book_register.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, url_for, session, redirect,flash
+from flask import Blueprint, render_template, request, url_for, session, redirect,flash
 
 import datetime
 
@@ -9,6 +9,30 @@ from lib.db import db
 
 book_register_bp = Blueprint('book_register', __name__)
 
+# 教科書登録確認画面を表示
+@book_register_bp.route('/book_register/confirm', methods=['POST'])
+def book_register_confirm():
+    if request.method == 'POST':
+        title=request.form.get('title')
+        user_id=session.get('user_id')
+        isbn_no=request.form.get('isbn_no')
+        author=request.form.get('author')
+        publisher=request.form.get('publisher')
+        price=request.form.get('price')
+        category=request.form.get('category')
+        status=request.form.get('status')
+
+        try:
+            return render_template('homes/book_register_confirm.html', title=title, user_id=user_id, isbn_no=isbn_no, author=author, publisher=publisher, price=price, category=category, status=status)
+        except:
+            flash('入力した値を再度確認してください', 'error')
+       
+
+
+
+        
+
+
 # 教科書登録処理
 @book_register_bp.route('/book_register/create', methods=['POST'])
 def book_register():
@@ -17,7 +41,7 @@ def book_register():
         user_id=session.get('user_id'),
         isbd_no=request.form.get('isbn_no'),
         author=request.form.get('author'),
-        publisher=request.form.get('company'),
+        publisher=request.form.get('publisher'),
         price=request.form.get('price'),
         category=request.form.get('category'),
         status=request.form.get('status'),
